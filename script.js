f17ab0f4054a2acdc7d64316a3742b687a9ca111
mohandand <mohandand@gmail.com>
2021-10-08 15:56:14 -0700

    updated code

--- script.js
+++ script.js
@@ -32,18 +32,22 @@ const quizData = [
         correct: "b",
     },
 ];
-const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit')
+const questionEl = document.getElementById('question');
+const submitBtn = document.getElementById('submit');
+const answerEls = document.querySelectorAll('.answer');
+
 
 let currentQuiz = 0 ;
+let totalScore= 0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers()
     const currentQuestion = quizData[currentQuiz]
     console.log(currentQuestion.question);
     questionEl.innerText = currentQuestion.question;
@@ -51,14 +55,49 @@ function loadQuiz() {
     b_text.innerText = currentQuestion.b;
     c_text.innerText = currentQuestion.c;
     d_text.innerText = currentQuestion.d;
+    const quiz = document.getElementById("quiz");
 }
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', Nextquestion);
 
-    currentQuiz++;
-    if(currentQuiz < quizData.length){
-          loadQuiz();
-           console.log(currentQuestion.question);
-      }else{
-          alert("Good Job!! You fineshed Quiz");
-      }
-});
+/*below function to get the selected radio button value by checking all radio values*/
+
+function getSelected(){
+    let answer = undefined;
+    answerEls.forEach((answerEL) => {
+        if(answerEL.checked){
+                answer =answerEL.id;
+        }
+
+    });
+       return answer;
+}
+
+//for deselcting answer before loading a question as by default it showing some answer
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+//Important function to check answer and load next question
+function Nextquestion(){
+
+    const answer = getSelected();
+    console.log(answer);
+    if(answer){
+        if(answer===quizData[currentQuiz].correct)
+        {
+            totalScore++;
+        }
+        currentQuiz++;
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        }else{
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${totalScore}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+}
